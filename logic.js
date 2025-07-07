13a70f768d103c63b7f416f6483bfa78a1469519
Jaycee Dela Rosa <88520055+Ciarra1@users.noreply.github.com.>
2025-07-07 22:22:41 +0800

    complete logic of the rock-paper-scissor game

---
+++ logic.js
@@ -0,0 +1,59 @@
+let computerScore = 0;
+let humanScore = 0;
+function getComputerChoice(){
+    let randomValue = Math.floor(Math.random()*99);
+    if(randomValue <= 33){
+        return "rock";
+    } else if (randomValue > 33 && randomValue <= 66){
+        return "paper";
+    } else {
+        return "scissor";
+    }
+}
+
+function getHumanChoice(){
+    let choice = prompt("Choose between: Rock, Paper, and Scissor");
+    return choice;
+}
+
+
+
+function playARound(){
+    let computerGuess = getComputerChoice();
+    let humanGuess = getHumanChoice();
+    console.log("Computer Guess: " +computerGuess);
+    console.log("Human Guess: " + humanGuess);
+    chooseWinner(humanGuess, computerGuess);
+    showScores();
+}
+function showScores(){
+    console.log("Human: " + humanScore);
+    console.log("Computer: " + computerScore);
+}
+function chooseWinner(humanChoice, computerChoice){
+        const outcomes = {
+            rock: {
+                rock: "Tie", paper: "Computer", scissor: "Human"
+            }, 
+            paper: {
+                rock: "Human", paper: "Tie", scissor: "Computer"
+            },
+            scissor: {
+                rock: "Computer", paper: "Human", scissor: "Tie"
+            }
+        }
+        let winner = outcomes[humanChoice][computerChoice];
+        if(winner === "Human"){
+            console.log("Human wins!");
+            humanScore+=1;
+        }else if (winner === "Computer"){
+            console.log("Computer wins!");
+            computerScore+=1;
+        } else if (winner === "Tie"){
+            console.log("Tie!");
+        }
+}
+for(let i = 0; i < 5; i++){
+
+    playARound();
+}
